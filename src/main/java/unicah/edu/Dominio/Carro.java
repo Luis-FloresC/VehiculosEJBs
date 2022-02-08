3754dbded9a0e0ca51bbad860424824415ac1dd4
Luis Flores <lf016158@gmail.com>
2022-02-07 23:33:01 -0600

    Se Creo la Clase de Carro

---
+++ src/main/java/unicah/edu/Dominio/Carro.java
@@ -0,0 +1,17 @@
+package unicah.edu.Dominio;
+
+import java.io.Serializable;
+
+public class Carro  implements Serializable {
+
+    private static final long serialVersionUIO = -6555504856528103960L;
+
+    private int idCarro;
+    private String Placa;
+    private String Marca;
+    private String Modelo;
+    private String Color;
+    private int Año;
+
+
+}
---
+++ src/main/java/unicah/edu/gp/gestionvehiculos/HelloApplication.java
@@ -0,0 +1,9 @@
+package unicah.edu.gp.gestionvehiculos;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
